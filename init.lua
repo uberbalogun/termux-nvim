25d57ecbe804b6554e93a5ff9a50f37d32229bbf
uberbalogun <baloggz@gmail.com>
2025-06-17 16:10:18 +0100

    Create init.lua
    initial commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- ~/.config/nvim/init.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.notify("Bootstrapping lazy.nvim...", vim.log.levels.INFO)
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("options")
+
+require("lazy").setup("plugins", {
+  checker = { enabled = true, notify = true },
+  change_detection = { enabled = true, notify = true },
+})
+
+vim.notify("Neovim configuration with lazy.nvim loaded!", vim.log.levels.INFO, { title = "Nvim" })
